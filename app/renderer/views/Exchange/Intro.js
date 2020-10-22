1f346c2d36e9fbe0c138d83ff1fc1a79a8c6f158
lightspeed393 <lightspeed393@protonmail.com>
2020-10-21 20:53:01 -0700

    Remove UTXO section from Exchange Intro.

--- app/renderer/views/Exchange/Intro.js
+++ app/renderer/views/Exchange/Intro.js
@@ -33,11 +33,6 @@ class Intro extends React.Component {
 					</p>
 					<p>Orders can take a while to propagate across the P2P network. You may need to wait a while for the
 						order book to display.</p>
-					<p>HyperDEX needs you to have correctly sized <ExternalLink
-						url="https://bitcoin.stackexchange.com/a/61579">UTXOs</ExternalLink> to make a successful swap.
-						For each UTXO you can make a single swap. The easiest way to ensure you can participate in a
-						swap is to make two or three smaller deposits to your account rather than a single large
-						deposit.</p>
 					<p>An atomic swap is comprised of six on-chain transactions. When trading a currency with high
 						transaction fees, it&apos;s important to slightly increase your price to account for the total
 						transaction fees. This is especially important when trading small amounts.</p>
